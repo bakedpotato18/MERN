23fdf51043e0d57d482ca244f30766fc6933fbdd
bakedpotato18 <amacarlson80@gmail.com>
2022-05-17 16:07:11 -0500

    Final

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,85 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Axios from 'axios';
 
 function App() {
   const[name, setName] = useState("");
   const[year, setYear] = useState(0);
   const[rating, setRating] = useState(0);
+  const[listOfBooks, setListOfBooks] = useState([]);
 
   const addBook = () => {
-    Axios.post('http://localhost:3001/addbook', {name: name, year: year, rating: rating}).then(() => {alert("It worked!";)} .catch(() => {alert("It failed!")}));
-  }
+    Axios.post('http://localhost:3001/addbook', {
+      name: name, 
+      year: year, 
+      rating: rating}).then(() => {
+    }).then(() => {
+      setListOfBooks([...listOfBooks, {name: name, year: year, rating: rating}])
+    })
+  };
+
+  const updateBook = (id) => {
+    const newYear = prompt("Enter new year");
+
+    Axios.put('http://localhost:3001/update', { newYear: newYear, id: id }).then(
+      () => {
+      setListOfBooks(
+        listOfBooks.map((val) => {
+        return val._id == id
+         ? {_id: id, name: val.name, year: newYear, rating: val.rating} : val;
+      }));
+  });
+};
+
+const deleteBook = (id) => {
+  Axios.delete(`http://localhost:3001/delete/${id}`).then(() => {
+    setListOfBooks(
+      listOfBooks.filter((val) => {
+      return val._id != id;
+    })
+    );
+  });
+};
+
+  useEffect(() => {
+    Axios.get('http://localhost:3001/read')
+    .then((response) => {
+      setListOfBooks(response.data);
+    })
+    .catch(() => {
+      console.log("ERR");
+    });
+  }, []);
 
   return (
     <div className="App">
       <div className="inputs">
+      <h2>Add Book</h2>
       <input type="text" placeholder="Book Name" onChange={(event) => {setName(event.target.value)}} />
       <input type="number" placeholder="Year Published" onChange={(event) => {setYear(event.target.value)}} />
       <input type="number" placeholder="Rating 1-5" onChange={(event) => {setRating(event.target.value)}} />
       <button onClick={addBook}>Add Book</button>
+      </div>
+      
+      <div className='container'>
+        <table>
+          <thead>
+            <tr>
+              <th colSpan="5">My Books</th>
+            </tr>
+          </thead>
+          <tbody>
+      {listOfBooks.map((val) => {
+        return <tr>
+                <td>{val.name}</td>
+                <td>{val.year}</td>
+                <td>{val.rating}</td>
+                <td><button onClick={() => {updateBook(val._id)}}>Update</button></td>
+                <td><button onClick={() => {deleteBook(val._id)}}>Delete</button></td>
+              </tr>
+      })}
+          </tbody>
+        </table> 
       </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 const BookModel = require('./models/Books');
+
+app.use(express.json());
+app.use(cors());
 
 ///Database Connection
 mongoose.connect(
@@ -9,21 +13,48 @@ mongoose.connect(
     { useNewUrlParser: true }
   );
 
-  app.get("/insert", async (req, res) => {
-    // const book = new BookModel({name: "This Book", year: 1980, rating: 5});
-    // await book.save();
-    // res.send("Inserted data.");
+  app.post("/addbook", async (req, res) => {
+    const name =  req.body.name;
+    const year = req.body.year;
+    const rating = req.body.rating;
+
+    const book = new BookModel({name: name, year: year, rating: rating});
+    await book.save();
+    res.send(book);
   });
 
-  app.post("/addbook", async (req, res) => {
-    BookModel.find({}, (err, result) =>{
-        if (err) {
-            res.send(err);
-        } else {
-            res.send(result);
-        }
+  app.get("/read", async (req, res) => {
+    BookModel.find({}, (err, result) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(result);
+      }
     });
   });
+
+  app.put("/update", async (req, res) => {
+    const newYear = req.body.newYear;
+    const id = req.body.id;
+    console.log(newYear, id);
+
+    try {
+      await BookModel.findById(id, (error, bookToUpdate) => {
+        bookToUpdate.year = Number(newYear);
+        bookToUpdate.save();
+      });
+    } catch (err) {
+      console.log(err);
+    }
+
+    res.send("Updated.");
+  });
+
+app.delete('/delete/:id', async (req, res) => {
+  const id = req.params.id;
+  await BookModel.findByIdAndRemove(id).exec();
+  res.send("Item deleted.");
+})
 
 app.listen(3001, () => {
     console.log("The server is running.");
